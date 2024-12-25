d9900c80157a95119aa4acd77d21b4f7a5fefc34
Tanmay337442 <tanmay337442@gmail.com>
2024-12-25 12:24:33 -0500

    test

--- app/app.py
+++ app/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, jsonify, redirect, url_for, make_response
 from flask_socketio import SocketIO, join_room, leave_room, emit
+import os
+print("Current working directory:", os.getcwd())
 import logic
 import uuid
 
